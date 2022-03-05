d37a8aa66490a4778b016969af846160f583bb92
Bojie Li <bojieli@gmail.com>
2022-03-05 13:07:18 +0800

    revise get data method

--- getdata.py
+++ getdata.py
@@ -3,13 +3,15 @@ from urllib.request import http
 import xml.etree.ElementTree as ET
 import datetime
 import os
+import json
 
 conn = http.client.HTTPConnection('www.gps902.net', port=6601)
 
+# load today's history
 headers = {
-	"Content-Type": "text/xml; charset=utf-8",
-	"SOAPAction": "http://tempuri.org/GetDevicesHistory",
-	"User-Agent": "TTWL/1.0 CFNetwork/1312 Darwin/21.0.0"
+        "Content-Type": "text/xml; charset=utf-8",
+        "SOAPAction": "http://tempuri.org/GetDevicesHistory",
+        "User-Agent": "TTWL/1.0 CFNetwork/1312 Darwin/21.0.0"
 }
 date = datetime.datetime.now().strftime("%Y/%m/%d")
 request_body = '''<?xml version="1.0" encoding="utf-8"?>
@@ -21,14 +23,17 @@ conn.request('POST', '/TutuAPI.asmx', body=request_body, headers=headers)
 response = conn.getresponse().read()
 xmltree = ET.fromstring(response)
 
+# create the today history file
 base_folder = os.path.join(os.path.dirname(__file__), 'data/')
 date = datetime.datetime.now().strftime("%Y%m%d")
 file_name = date + '.json'
 file_path = os.path.join(base_folder, file_name)
-f = open(base_folder + file_name, mode='w')
-f.write(xmltree[0][0][0].text)
+f = open(file_path, mode='w')
+today_history = xmltree[0][0][0].text
+f.write(today_history)
 f.close()
 
+# load read-time status
 headers["SOAPAction"] = "http://tempuri.org/GetTracking"
 request_body = '''<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body>
@@ -37,9 +42,43 @@ request_body = '''<?xml version="1.0" encoding="utf-8"?>
 conn.request('POST', '/TutuAPI.asmx', body=request_body, headers=headers)
 response = conn.getresponse().read()
 
+# create the real-time status file
 xmltree = ET.fromstring(response)
 file_name = 'status.json'
 file_path = os.path.join(base_folder, file_name)
-f = open(base_folder + file_name, mode='w')
-f.write(xmltree[0][0][0].text)
+f = open(file_path, mode='w')
+curr_status = xmltree[0][0][0].text
+f.write(curr_status)
 f.close()
+
+# create the final today history file
+file_name = 'status-' + date + '.json'
+file_path = os.path.join(base_folder, file_name)
+datetimes = dict()
+try:
+    with open(file_path, mode='r') as f:
+        data = f.read()
+        jsondata = json.loads(data)
+        for point in jsondata:
+            if point["positionTime"] not in datetimes:
+                datetimes[point["positionTime"]] = point
+except:
+    pass
+
+new_point = json.loads(curr_status)
+if new_point["positionTime"] not in datetimes:
+    datetimes[new_point["positionTime"]] = new_point
+
+try:
+    history_points = json.loads(today_history)['devices']
+    for point in history_points:
+        if point["pt"] not in datetimes:
+            point["positionTime"] = point["pt"]
+            point.pop("pt")
+            datetimes[point["positionTime"]] = point
+except:
+    pass
+
+dedup_data = [datetimes[key] for key in sorted(datetimes)]
+with open(file_path, mode='w') as f:
+    f.write(json.dumps(dedup_data))
